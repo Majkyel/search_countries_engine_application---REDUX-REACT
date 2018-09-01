d26c5c015069048d26eb06e681a81abef7f04cb8
Michał Kuliś <michalkulis1993@gmail.com>
2018-09-01 16:30:10 +0200

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,16 @@ import Error from './presentational/notfound.component.js';
 import Home from './presentational/home.component.js';
 import Contact from './presentational/contact.component.js';
 import CountryFlagContainer from './containers/flag-container.component.js';
+import CountryDetailsContainer from './containers/country-details-container.component.js';
 
 
 export default (
     <Route path='/' component={Navigation}>
         <IndexRoute component={Home}/>
-        <Route path='countries' component={CountryFlagContainer}/>
+        <Route path='countries'>
+            <IndexRoute component={CountryFlagContainer}/>
+            <Route path='country:id' component={CountryDetailsContainer}/>
+        </Route>
         <Route path='contact' component={Contact}/>
         <Route path='*' component={Error}/>
     </Route>
