bf1cfac3c87555cae05012ec27df449a776ebca5
Michał Kuliś <michalkulis1993@gmail.com>
2018-09-01 16:29:37 +0200

    Update reducer.js and index.js in reducers/ folder

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,20 +1,29 @@
-import {GET_COUNTRIES} from '../actions/actions.js';
+import {GET_COUNTRIES, GET_COUNTRY, SEARCH_COUNTRIES, DELETE_COUNTRY} from '../actions/actions.js';
 import countriesData from '../data/countries.json';
 
 const initialState = {
-    countries: countriesData
+    countries: countriesData,
+    selectedCountry: {},
+    visibleCountries: [] //countriesData ??
 };
 
 
 const countriesReducer = function(state = initialState, action) {
     switch(action.type) {
         case GET_COUNTRIES:
-            return Object.assign({}, state, {countries: state.countries})
-        default:
-            return state;
+            return Object.assign({}, state, {countries: state.countries});
+        case GET_COUNTRY:
+            const selectedCountry = state.countries.find(country => country.id === parseInt(action.id));
+            return Object.assign({}, state, {selectedCountry});
+        case SEARCH_COUNTRIES:
+            const foundCountries = state.countries.filter(country => country.name.toLowerCase().includes(action.searchText.toLowerCase()));
+            return Object.assign({}, state, {visibleCountries: foundCountries});
+        case DELETE_COUNTRY:
+            const notDeleteCountries = state.countries.filter(counter => counter.id !== action.id);
+            const notDeleteVisibleCountries = state.visibleCountries.filter(country => country.id !== action.id);
+            return Object.assign({}, state, {countries: notDeleteCountries, visibleCountries: notDeleteVisibleCountries});
     }
-    
-    //return state;
+    return state;
 };
 
 export default countriesReducer;
